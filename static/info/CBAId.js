7b6ed266b70c31b9037c9cb6e7fd91336c57d2a3
fanke007 <706534607@qq.com>
2019-03-27 10:22:37 +0800

    增加项目

---
+++ static/info/CBAId.js
@@ -0,0 +1,80 @@
+export const Url = {
+  url: "https://i1.hoopchina.com.cn/touch/907/151212357827907/touch_home_151212357827907",
+  url2:"https://c2.hoopchina.com.cn/uploads/star/event/images",
+  url3:"https://c1.hoopchina.com.cn/uploads/star/event/images"
+};
+export const CBAlist=[
+  {
+    "id":"id1",
+    "src":"https://c2.hoopchina.com.cn/uploads/star/event/images/190311/ac8c5d473dd3493a4c57d5196a7fe2ccd05c894f.jpg@.webp",
+  "title":"球队征程临近结束，李荣培：可惜没能陪队伍走到最后",
+    "from":"虎扑CBA",
+    "time":"4 小时前"
+  },{
+    "id":"id2",
+    "src":Url.url2+"/190311/ff20c6561d7f379282ddda085e973f8fe2050fb8.jpg@.webp",
+    "title":"广东官方：易建联因伤休息一周，缺席常规赛收官战",
+    "from":"虎扑CBA",
+    "time":"3 小时前"
+  },{
+    "id":"id3",
+    "src":Url.url2+"/190311/6019c96f451c0937f7bcefeafc6c722e2b6a119e.jpg@.webp",
+    "title":"莫泰谦虚回应获月最佳：离不开队友及球迷的支持",
+    "from":"虎扑CBA",
+    "time":"5 小时前"
+  },{
+    "id":"id4",
+    "src":Url.url3+"/190311/164162fce3150a9ccc52884fd64de5095fcd4d46.jpg@.webp",
+    "title":"张德贵：缺乏优质本土球员，外援不力导致阵容不够稳定",
+    "from":"北京日报",
+    "time":"6 小时前"
+  },{
+    "id":"id5",
+    "src":Url.url2+"/190311/f62f1f33495cf60172228e42be18552deedd328e.jpg@.webp",
+    "title":"胡明轩生日感悟：冤冤相报何时了",
+    "from":"虎扑CBA",
+    "time":"6 小时前"
+  },{
+    "id":"id6",
+    "src":Url.url2+"/190311/996049c250276ba4194212cb25419b6ca4628ec4.jpg@.webp",
+    "title":"第4期月最佳球员：郭艾伦、莫泰尤纳斯携手当选",
+    "from":"虎扑CBA",
+    "time":"6 小时前"
+  },{
+    "id":"id1",
+    "src":"https://c2.hoopchina.com.cn/uploads/star/event/images/190311/ac8c5d473dd3493a4c57d5196a7fe2ccd05c894f.jpg@.webp",
+  "title":"球队征程临近结束，李荣培：可惜没能陪队伍走到最后",
+    "from":"虎扑CBA",
+    "time":"4 小时前"
+  },{
+    "id":"id2",
+    "src":Url.url2+"/190311/ff20c6561d7f379282ddda085e973f8fe2050fb8.jpg@.webp",
+    "title":"广东官方：易建联因伤休息一周，缺席常规赛收官战",
+    "from":"虎扑CBA",
+    "time":"3 小时前"
+  },{
+    "id":"id3",
+    "src":Url.url2+"/190311/6019c96f451c0937f7bcefeafc6c722e2b6a119e.jpg@.webp",
+    "title":"莫泰谦虚回应获月最佳：离不开队友及球迷的支持",
+    "from":"虎扑CBA",
+    "time":"5 小时前"
+  },{
+    "id":"id4",
+    "src":Url.url3+"/190311/164162fce3150a9ccc52884fd64de5095fcd4d46.jpg@.webp",
+    "title":"张德贵：缺乏优质本土球员，外援不力导致阵容不够稳定",
+    "from":"北京日报",
+    "time":"6 小时前"
+  },{
+    "id":"id5",
+    "src":Url.url2+"/190311/f62f1f33495cf60172228e42be18552deedd328e.jpg@.webp",
+    "title":"胡明轩生日感悟：冤冤相报何时了",
+    "from":"虎扑CBA",
+    "time":"6 小时前"
+  },{
+    "id":"id6",
+    "src":Url.url2+"/190311/996049c250276ba4194212cb25419b6ca4628ec4.jpg@.webp",
+    "title":"第4期月最佳球员：郭艾伦、莫泰尤纳斯携手当选",
+    "from":"虎扑CBA",
+    "time":"6 小时前"
+  }
+]
---
+++ static/info/info.js
@@ -0,0 +1,113 @@
+export const Url = {
+  url: "https://i1.hoopchina.com.cn/touch/907/151212357827907/touch_home_151212357827907",
+  url2:"https://c2.hoopchina.com.cn/uploads/star/event/images",
+  url3:"https://c1.hoopchina.com.cn/uploads/star/event/images"
+};
+// 新闻首页banner
+export const banner=[
+  {
+    text:"海沃德对勇士30+7+4集锦：三分球6中4",
+    src:"https://i3.hoopchina.com.cn/blogfile/201903/05/BbsImg_249719736070838_1551774455_s_58049_o_w_782_h_438_96512.jpg"
+  },
+  {
+    text:"武切维奇：低位猛兽，进击不止",
+    src:"https://bbsstaticoss.hoopchina.com.cn/aHR0cHM6Ly9pMTEuaG9vcGNoaW5hLmNvbS5jbi9odXB1YXBwL2Jicy80MTIvMjE2ODI0MTIvdGhyZWFkXzIxNjgyNDEyXzIwMTkwMzA0MDczOTAwX3NfMjU3MzI3X3dfNzU2X2hfNDk1Xzg0MzYucG5n"
+  },
+  {
+    text:"红魔半场伤三人,曼联0-0惨平利物浦",
+    src:"https://i1.hoopchina.com.cn/blogfile/201902/25/BbsImg_232426160050445_1551024774_s_50130_o_w_653_h_350_72793.jpg"
+  },
+]
+//首页导航信息
+
+
+export  const navlist=[
+  {
+    name:"NBA",
+    path:"NBA",
+    id:"NBA"
+  },
+  {
+    name:"CBA",
+    path:"CBA",
+    id:"CBA"
+  },
+  {
+    name:"足球",
+    path:"soccer",
+    id:"soccer"
+  },
+  {
+    name:"电竞",
+    path:"gg",
+    id:"gg"
+  },
+  {
+    name:"社区",
+    path:"bbc",
+    id:"bbc"
+  },
+]
+//NBA页新闻列表
+export const matchBoard=[
+  {
+    title:"【Game65热线】03月08日9:00对阵步行者|醒醒兄弟",
+    reply:"1亮20回复"
+  },
+  {
+    title:"【G65】 波特兰雷拓四番战",
+    reply:"6亮72回复"
+  },
+  {
+    title:"爵士官方发图祝贺米切尔当选西部周最佳球员",
+    reply:"1亮20回复"
+  },
+  {
+    title:"尼克斯正式无缘季后赛，成本赛季东部首支无缘季后赛球队",
+    reply:"12回复"
+  },
+  {
+    title:"76人官方发图祝贺西蒙斯当选东部周最佳球员",
+    reply:"20回复"
+  },
+  {
+    title:"兰德尔全场30分8板5助攻，末节发力砍下14分",
+    reply:"1亮20回复"
+  },{
+    title:"【Game65热线】03月08日9:00对阵步行者|醒醒兄弟",
+    reply:"20回复"
+  },{
+    title:"卡莱尔：波尔津吉斯有很多优秀特质，首要目标是恢复健康",
+    reply:"1亮20回复"
+  },{
+    title:"【Game65热线】03月08日9:00对阵步行者|醒醒兄弟",
+    reply:"9亮20回复"
+  },{
+    title:"官方：火箭与猛龙将于10月参加2019NBA日本季前赛",
+    reply:"5亮20回复"
+  },{
+    title:"[赛后]凯尔特人111-109国王，塔特姆24分，霍弗德21+11+7，海沃德绝杀，巴恩斯24+8",
+    reply:"1亮20回复"
+  },{
+    title:"【Game65热线】03月08日9:00对阵步行者|醒醒兄弟",
+    reply:"7亮20回复"
+  },{
+    title:"博古特：希望能再添一枚戒指，期待和考辛斯合作",
+    reply:"8亮60回复"
+  },{
+    title:"官方：虎扑招聘NBA、CBA新闻实习生",
+    reply:""
+  },
+]
+
+// 赛程
+export const match=[
+  {
+    teamNameA:"步行者",
+    teamNameB:"雄鹿",
+    logoA:"//gdc.hupucdn.com/gdc/nba/team/logo/123x124/83b033b5c036f590.png",
+    logoB:"//gdc.hupucdn.com/gdc/nba/team/logo/123x124/1d3c3c7503cac4a6.png",
+    score:"64 - 72",
+    time:"第三节剩5:06"
+  }
+]
